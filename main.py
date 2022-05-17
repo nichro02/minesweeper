1692d2713e910c350f1ba1ba5d0f9cfa658b99b5
nichro02 <nichro02@gmail.com>
2022-05-16 18:39:09 -0600

    Replaced hard-coded values with height and width util functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import settings
+import utils
 
 #instatiate window instance
 root = Tk()
@@ -16,18 +17,18 @@ root.resizable(False, False)
 top_frame = Frame(
     root,
     bg='red',
-    width=1440,
-    height=180
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
     bg='blue',
-    width=360,
-    height=540
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
 )
-left_frame.place(x=0,y=180)
+left_frame.place(x=0,y=utils.height_prct(25))
 
 #run the window
 root.mainloop()
--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ def height_prct(percentage):
     return (settings.HEIGHT/100) * percentage
 
 print(height_prct(25))
+
+def width_prct(percentage):
+    return (settings.WIDTH/100) * percentage
