7bb3174f289f2b0ed838ba4c4fb6a3292d2129e0
nichro02 <nichro02@gmail.com>
2022-05-16 18:44:16 -0600

    Created frame to house game board

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ root.resizable(False, False)
 #create frames to structure window
 top_frame = Frame(
     root,
-    bg='red',
+    bg='black',
     width=settings.WIDTH,
     height=utils.height_prct(25)
 )
@@ -24,11 +24,22 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='blue',
+    bg='black',
     width=utils.width_prct(25),
     height=utils.height_prct(75)
 )
 left_frame.place(x=0,y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25)
+)
 
 #run the window
 root.mainloop()
