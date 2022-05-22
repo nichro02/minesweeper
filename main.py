6d8adc6c4c4c395fbf722e73f4c2495a7df02693
nichro02 <nichro02@gmail.com>
2022-05-21 18:22:23 -0600

    Refactored main.py to leverage grid instead of place when creating game

--- main.py
+++ main.py
@@ -46,9 +46,16 @@ center_frame.place(
 #instantiate game cells
 c1 = Cell()
 c1.create_btn_obj(center_frame)
-c1.cell_btn_object.place(
-    x=0,
-    y=0
+c1.cell_btn_object.grid(
+    column=0,
+    row=0
+)
+
+c2 = Cell()
+c2.create_btn_obj(center_frame)
+c2.cell_btn_object.grid(
+    column=0,
+    row=1
 )
 
 #run the window
